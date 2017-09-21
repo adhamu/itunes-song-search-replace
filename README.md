0bd65086bc7752425d4cf473838354359f7ef907
Amit Dhamu <dev@amitd.co>
2017-09-21 21:49:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# iTunes Song Name Replacer
+Get rid of annoying strings in MP3s song names. Inspired by [Doug's Script for Search & Replace](https://dougscripts.com/itunes/scripts/ss.php?sp=searchreplacetagtext).
+
+## Requirements
+- iTunes (need your library file)
+- Python3
+
+## Example Usage
+
+```python
+python3 index.py -i 'iTunes Music Library.xml' -s 'feat.' -r 'Featuring '
+```
+
+## What Else
+By default, the first time you load your library, it will be cached using Pickle. Useful if you have a large library. To disable this, add the `--nocache` flag when you execute the script.
+
+## FYI
+[If you don't see an .xml library file in ~/Music/iTunes, you probably started using iTunes after version 12.2, and have never enabled sharing between iTunes and other apps. To generate one, go to iTunes Preferences | Advanced and select "Share iTunes Library XML with other applications." (Apple docs)](https://github.com/liamks/libpytunes#libpytunes).
---
+++ index.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import pickle
+import os
+import time
+from mutagen.easyid3 import EasyID3
+from libpytunes import Library
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Searches your iTunes library and replaces an offending string'
+    )
+    parser.add_argument(
+        '-i',
+        metavar='iTunes Library',
+        type=str,
+        required=True,
+        help='Absolute path to iTunes Library XML file'
+    )
+    parser.add_argument(
+        '-s',
+        metavar='Search term',
+        type=str,
+        required=True,
+        help='The string you are searching for'
+    )
+    parser.add_argument(
+        '-r',
+        metavar='Replace with',
+        type=str,
+        required=True,
+        help='What you want to replace it with'
+    )
+    parser.add_argument(
+        '--nocache',
+        action='store_true',
+        help='Disable caching with pickle'
+    )
+
+    args = parser.parse_args()
+
+    itunes_library = os.path.realpath(args.i)
+    offence = args.s
+    replace_offence_with = args.r
+    confirm_message = 'Replace "' + offence + '" with ' + '"' + replace_offence_with + '" [y/n] ? '
+    found = False
+
+    lib_path = itunes_library
+    pickle_file = 'itl.p'
+    expiry = 0 if args.nocache else (60 * 60)
+    epoch_time = int(time.time())
+
+    if not os.path.isfile(pickle_file) or os.path.getmtime(pickle_file) + expiry < epoch_time:
+        itl_source = Library(lib_path)
+        pickle.dump(itl_source, open(pickle_file, 'wb'))
+
+    itl = pickle.load(open(pickle_file, 'rb'))
+
+    for id, song in itl.songs.items():
+        if song.name.find(offence) != -1:
+            found = True
+            print('/' + song.location)
+
+            confirmation = input(confirm_message)
+
+            if confirmation == 'y':
+                try:
+                    audio = EasyID3('/' + song.location)
+                    audio['title'] = song.name.replace(offence, replace_offence_with)
+                    audio.save()
+                    print('Saved as ' + audio['title'][0])
+                except Exception as e:
+                    print('File not found. Try clearing your cache')
+            else:
+                print('Song name not changed')
+
+    if found is False:
+        print('No results found')
